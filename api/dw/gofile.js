458ad4e1f779e1694bf7b34184d8e85b66f86477
tapr5 <rayanchariy667@gmail.com>
2025-10-20 17:27:23 +0100

    Create gofile.js
    Signed-off-by: tapr5 <rayanchariy667@gmail.com>

---
+++ api/dw/gofile.js
@@ -0,0 +1,38 @@
+// pages/api/gofile.js
+import axios from "axios";
+
+export default async function handler(req, res) {
+  try {
+    const url =
+      "https://api.gofile.io/contents/iZPZN3?wt=4fd6sg89d7s6&contentFilter=&page=1&pageSize=1000&sortField=name&sortDirection=1";
+
+    const headers = {
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br, zstd",
+      "Accept-Language": "fr-FR,fxq=0.7",
+      Authorization: "Bearer KP7yA28ChZttCtRCfHwq2J5m8ud2jXfh",
+      Connection: "keep-alive",
+      Host: "api.gofile.io",
+      Origin: "https://gofile.io",
+      Referer: "https://gofile.io/",
+      "Sec-Fetch-Dest": "empty",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Site": "same-site",
+      "Sec-GPC": "1",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
+    };
+
+    // تنفيذ الطلب
+    const { data } = await axios.get(url, { headers });
+
+    // إرجاع النتيجة كـ JSON
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("❌ خطأ في جلب بيانات GoFile:", error.message);
+    res.status(500).json({
+      error: "فشل في جلب البيانات من GoFile",
+      details: error.message,
+    });
+  }
+}
