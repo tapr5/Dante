b9d42226a22c7f530847881e6725a69cac8cd711
tapr5 <rayanchariy667@gmail.com>
2025-11-06 12:21:12 +0100

    Update witanimes.js
    Signed-off-by: tapr5 <rayanchariy667@gmail.com>

--- api/witanime/witanimes.js
+++ api/witanime/witanimes.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 
 const BASE_URL = "https://witanime.you/";
-const PROXY_URL = "https://api.allorigins.win/raw?url=";
+const PROXY_URL = "http://217.154.201.164:7763/api/proxy?url=";
 const HEADERS = {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
